3836f211f5e7ec6317384f27aecae25065aafac1
bernatferragut <elbernat@gmail.com>
2018-09-04 16:48:16 -0400

    update

--- GENART/GENART-10/script.js
+++ GENART/GENART-10/script.js
@@ -1,74 +1,31 @@
-window.onload = function() {
-  
-    // getting the CANVAS
-    let canvas = document.querySelector('canvas');
-    // setting the canvas context
-    let ctx = canvas.getContext('2d');
-    // getting canvas size
-    canvas.width  = window.innerWidth;
-    canvas.height = window.innerHeight;
-    // color and size
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
-    // global vars
-    let colors = ['#8DDDF7','#B1A7F2','#B1A7F2','#E9A1F7','#AC77E4'];
-    
-    // *** Particle Object ***
-    // Attributes
-    let Particle = function() {
-      this.message= 'particle control';
-      this.size = 5;
-      this.posX = 100;
-      this.posY = 100;
-      this.velX=1;
-      this.velY = 1;
-      this.gravity = 0.1;
-      this.colors = colors;
-      }
-    // Methods
-    Particle.prototype.draw = function(velX) {
-      // particle
-      let rnd = Math.floor(Math.random()*5);
-      ctx.fillStyle = colors[rnd];
-      ctx.fillRect(this.posX, this.posY, this.size, this.size);
-      // step
-      this.posX += this.velX;
-      this.posY += this.velY;
-      this.velY += this.gravity;
-      // rebounding on both axes
-      if(this.posX+this.size > canvas.width || this.posX-this.size < 0)  {
-        this.velX = -this.velX
-      }
-      if(this.posY+this.size > canvas.height || this.posY-this.size < 0)  {
-        this.velY = -this.velY
-      }
-    }
-    // *** Particle Object ***
-    let p = new Particle();
-    
-    // GUI particle control :)
-    let gui = new dat.GUI();
-    gui.add(p, 'message');
-    gui.add(p, 'velX', -20, 20);
-    gui.add(p, 'velY', -20, 20);
-    gui.add(p, 'gravity', -1,1);
-    
-    // *** Animation ***
-    function animate() {
-      // animation function
-      requestAnimationFrame(animate)
-      // erase canvas
-      ctx.fillStyle = 'rgba(0,0,0,0.1)';
-      ctx.fillRect(0, 0, canvas.width, canvas.height);
-      p.draw();
-    }
-    animate();
-  
+// JS with P5JS
+'use strict';
+
+let props = {
+  canvas : {
+    w : window.innerWidth,
+    h : window.innerHeight,
+    spacing : 5
+  },
+  circle : {
+    size : 2
   }
-  
-  
-  
-  
-  
-  
+}
+
+function setup() {
+  // CANVAS
+  createCanvas(props.canvas.w, props.canvas.h);
+  background('black');
+  // POPULATE
+  for(let x=0; x<props.canvas.w; x += props.canvas.spacing) {
+    for(let y=0; y<props.canvas.h; y += props.canvas.spacing ) {
+      noStroke();
+      fill(color(random(255),0,0));
+      ellipse(x,y,props.circle.size, props.circle.size)
+    }
+  }
+}
+
+function draw() {
+  // CODE  
+}
